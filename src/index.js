723439592b225d61699a9461f94b9d8dead811c0
erik-kers <erik.kers.ohrnell@gmail.com>
2021-03-26 12:57:08 +0100

    Remove theme for later date

--- src/index.js
+++ src/index.js
@@ -1,30 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createMuiTheme, MuiThemeProvider, StylesProvider } from '@material-ui/core/styles';
-import { ThemeProvider } from 'styled-components';
 import App from './App';
-
-const theme = createMuiTheme({
-  palette: {
-    type: 'dark',
-    primary: {
-      main: '#eaeae7',
-    },
-    secondary: {
-      main: '#00e676',
-    },
-  },
-});
 
 ReactDOM.render(
   <React.StrictMode>
-    <StylesProvider injectFirst>
-      <MuiThemeProvider theme={theme}>
-        <ThemeProvider theme={theme}>
-          <App />
-        </ThemeProvider>
-      </MuiThemeProvider>
-    </StylesProvider>
+    <App />
   </React.StrictMode>,
 
   document.getElementById('root')
